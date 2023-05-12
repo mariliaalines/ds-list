2f394b6c263c315a3288cf0007db10ab6e90808e
Marilia Santiago <marilia.aline18@gmail.com>
2023-05-12 20:15:52 -0300

    Database seed update

---
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -0,0 +1,38 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_game_list")
+public class GameList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    public GameList(){
+
+    }
+
+    public GameList(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GameList gameList)) return false;
+        return Objects.equals(id, gameList.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+
+}
