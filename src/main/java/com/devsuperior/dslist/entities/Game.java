65020d744464d00c42959ad1144fb3fc931356c5
Marilia Santiago <marilia.aline18@gmail.com>
2023-05-11 22:47:30 -0300

    GameMinDTO, GameRepository, GameService, GameController

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -39,6 +39,42 @@ public class Game {
         this.longDescription = longDescription;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o)
