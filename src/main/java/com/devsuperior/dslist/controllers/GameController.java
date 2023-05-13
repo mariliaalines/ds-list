cb8c346bbebea963a0767ea83eb349c0631e5920
Marilia Santiago <marilia.aline18@gmail.com>
2023-05-12 23:43:45 -0300

    Implementation of search for games by list

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,9 +1,13 @@
 package com.devsuperior.dslist.controllers;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.service.GameListService;
+import com.devsuperior.dslist.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +18,17 @@ import java.util.List;
 public class GameController {
 
     @Autowired
-    private GameListService gameListService;
+    private GameService gameService;
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
 
     @GetMapping
-    public List<GameListDTO> findAll(){
-        var result = gameListService.findAll();
+    public List<GameMinDTO> findAll(){
+        var result = gameService.findAll();
         return result;
     }
 
