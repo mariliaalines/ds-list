16623b6252886ff872e540a9652b001df1aea4be
Marilia Santiago <marilia.aline18@gmail.com>
2023-05-14 13:53:31 -0300

    Implementation move games on list

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,15 @@
+package com.devsuperior.dslist.dto;
+
+public class ReplacementDTO {
+
+    private Integer sourceIndex;
+    private Integer destinationIndex;
+
+    public Integer getSourceIndex() {
+        return sourceIndex;
+    }
+
+    public Integer getDestinationIndex() {
+        return destinationIndex;
+    }
+}
